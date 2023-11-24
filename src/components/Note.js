3b7acf4d4b612853bf783374d48610d66dbec17d
Arnaud Renaud <arnaud.renaud@gmail.com>
2023-11-24 16:57:12 +0100

    Display note title and content in form

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,35 @@
 import { useParams } from "react-router-dom";
 
 import "./Note.css";
+import { useEffect, useState } from "react";
 
 function Note() {
   const { id } = useParams();
 
+  const [note, setNote] = useState(null);
+
+  async function fetchNote() {
+    const response = await fetch(`/notes/${id}`);
+    const data = await response.json();
+    setNote(data);
+  }
+
+  useEffect(() => {
+    fetchNote();
+  }, [id]);
+
+  if (!note) {
+    return "Chargement…";
+  }
+
   return (
     <form className="Form">
-      <input className="Note-editable Note-title" type="text" />
-      <textarea className="Note-editable Note-content" />
+      <input
+        className="Note-editable Note-title"
+        type="text"
+        value={note.title}
+      />
+      <textarea className="Note-editable Note-content" value={note.content} />
       <div className="Note-actions">
         <button className="Button">Enregistrer</button>
       </div>
